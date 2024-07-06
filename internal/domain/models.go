4ec35af8c92074cd73e32ab87f00baeda5368977
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 17:33:12 +0300

    init entities

---
+++ internal/domain/models.go
@@ -0,0 +1,23 @@
+package domain
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type UserDTO struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type UserAuthDTO struct {
+	Nickname string `json:"nickname"`
+	Password string `json:"password"`
+}
