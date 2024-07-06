309e44f6e4662c4f05b8be9e5293d7a91a42cdfe
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 20:30:05 +0300

    add jwtUtil

--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -3,43 +3,24 @@ package usecase
 import (
 	"AuthService/internal/config"
 	"AuthService/internal/domain"
+	"AuthService/internal/utils"
 	"context"
 	"crypto/sha1"
 	"fmt"
-	"github.com/golang-jwt/jwt/v5"
-	"time"
 )
 
 type authService struct {
-	repo domain.UserRepository
-	cfg  *config.Config
+	repo    domain.UserRepository
+	jwtUtil *utils.JWTUtil
+	cfg     *config.Config
 }
 
-type jwtClaims struct {
-	Nickname string `json:"email"`
-	jwt.RegisteredClaims
-}
-
-func NewAuthService(repo domain.UserRepository, cfg *config.Config) UseCase {
+func NewAuthService(repo domain.UserRepository, jwtUtil *utils.JWTUtil, cfg *config.Config) AuthUseCase {
 	return &authService{
-		repo: repo,
-		cfg:  cfg,
+		repo:    repo,
+		jwtUtil: jwtUtil,
+		cfg:     cfg,
 	}
-}
-
-func (s *authService) generateAuthToken(user *domain.User) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwtClaims{
-		Nickname: user.Nickname,
-		RegisteredClaims: jwt.RegisteredClaims{
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-		},
-	})
-	tokenStr, err := token.SignedString([]byte(s.cfg.JWT.Secret))
-	if err != nil {
-		return "", err
-	}
-	return tokenStr, nil
 }
 
 func (s *authService) generatePasswordHash(password string) string {
@@ -54,7 +35,7 @@ func (s *authService) SignIn(ctx context.Context, dto *domain.UserAuthDTO) (stri
 	if err != nil {
 		return "", err
 	}
-	token, err := s.generateAuthToken(user)
+	token, err := s.jwtUtil.GenerateToken(user)
 	if err != nil {
 		return "", err
 	}
