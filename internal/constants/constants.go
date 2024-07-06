8e42a20fbfb238aef2ffe14de9b8ed36bce0dd69
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 20:29:46 +0300

    add constant roles and claims

---
+++ internal/constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	AdminRoleName   string = "admin"
+	DefaultRoleName string = "user"
+
+	AuthorizationHeaderKey string = "Authorization"
+	UserIdKey              string = "UserID"
+	NicknameKey            string = "Nickname"
+	EmailKey               string = "Email"
+	ExpireTimeKey          string = "Exp"
+)
