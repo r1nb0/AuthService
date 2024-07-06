37b62c944664a7b910c678e30e5b142101450398
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 22:23:43 +0300

    add user usecase interface

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -9,3 +9,9 @@ type AuthUseCase interface {
 	SignIn(ctx context.Context, dto *domain.UserAuthDTO) (string, error)
 	SignUp(ctx context.Context, dto *domain.UserDTO) (int, error)
 }
+
+type UserUseCase interface {
+	GetAll(ctx context.Context) ([]*domain.User, error)
+	GetByID(ctx context.Context, id int) (*domain.User, error)
+	Update(ctx context.Context, id int, user *domain.UserDTO) error
+}
