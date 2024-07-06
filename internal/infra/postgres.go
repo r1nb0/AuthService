24c8ac5f0867cb6266f17da7823fee80713f61d6
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 14:26:51 +0300

    init postgres

---
+++ internal/infra/postgres.go
@@ -0,0 +1,29 @@
+package infra
+
+import (
+	"AuthService/config"
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+func InitPostgres(cfg *config.Config) (*sqlx.DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	dataSourceName := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Postgres.Host, cfg.Postgres.Port,
+		cfg.Postgres.Username, cfg.Postgres.Password,
+		cfg.Postgres.DBName, cfg.Postgres.SSLMode,
+	)
+	db, err := sqlx.ConnectContext(ctx, "postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
