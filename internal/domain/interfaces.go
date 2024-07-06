062500303e759c9227de97823a0025e8f527ed33
r1nb0 <ilia.mesha12@gmail.com>
2024-07-06 22:23:23 +0300

    add update & get methods

--- internal/domain/interfaces.go
+++ internal/domain/interfaces.go
@@ -3,6 +3,9 @@ package domain
 import "context"
 
 type UserRepository interface {
-	CreateUser(ctx context.Context, dto *UserDTO) (int, error)
-	GetUser(ctx context.Context, dto *UserAuthDTO) (*User, error)
+	Create(ctx context.Context, dto *UserDTO) (int, error)
+	GetByAuthData(ctx context.Context, dto *UserAuthDTO) (*User, error)
+	GetAll(ctx context.Context) ([]*User, error)
+	GetByID(ctx context.Context, id int) (*User, error)
+	Update(ctx context.Context, id int, dto *UserDTO) error
 }
