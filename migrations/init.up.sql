06e8775a05cdf9dd10c2a3615bc0f4443cc8be4b
r1nb0 <ilia.mesha12@gmail.com>
2024-07-07 17:19:36 +0300

    add nickname index

--- migrations/init.up.sql
+++ migrations/init.up.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS users
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    nickname TEXT NOT NULL,
+    nickname TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_email ON users (email);
+CREATE INDEX IF NOT EXISTS idx_nickname ON users (nicknamea);
