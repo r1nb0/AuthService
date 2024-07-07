815adb8448f7c5bd5dab5c6fd00f773fb5cd00af
r1nb0 <ilia.mesha12@gmail.com>
2024-07-07 16:38:09 +0300

    impl Logger interface

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -1,1 +1,13 @@
 package logging
+
+// Logger
+// TODO (impl Infof, Debugf, Warnf, Errorf, Fatalf)
+type Logger interface {
+	Init()
+
+	Info(cat Category, sub SubCategory, msg string, another map[string]interface{})
+	Debug(cat Category, sub SubCategory, msg string, another map[string]interface{})
+	Warn(cat Category, sub SubCategory, msg string, another map[string]interface{})
+	Error(cat Category, sub SubCategory, msg string, another map[string]interface{})
+	Fatal(cat Category, sub SubCategory, msg string, another map[string]interface{})
+}
