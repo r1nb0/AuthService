c8e48d9331972393aa2b6b3518e7ede439cf1a25
r1nb0 <ilia.mesha12@gmail.com>
2024-07-07 16:37:05 +0300

    add requirements for validator

--- internal/domain/models.go
+++ internal/domain/models.go
@@ -10,14 +10,14 @@ type User struct {
 }
 
 type UserDTO struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Nickname  string `json:"nickname"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
+	FirstName string `json:"first_name" validate:"required,min=3"`
+	LastName  string `json:"last_name" validate:"required,min=3"`
+	Nickname  string `json:"nickname" validate:"required,min=3"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=6,max=64"`
 }
 
 type UserAuthDTO struct {
-	Nickname string `json:"nickname"`
-	Password string `json:"password"`
+	Nickname string `json:"nickname" validate:"required,min=3"`
+	Password string `json:"password" validate:"required,min=6,max=64"`
 }
