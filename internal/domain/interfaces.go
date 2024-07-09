386f6721af6f5a10ed3a6cbfab6b8d18e189e216
r1nb0 <ilia.mesha12@gmail.com>
2024-07-09 17:33:18 +0300

    removed exists methods

--- internal/domain/interfaces.go
+++ internal/domain/interfaces.go
@@ -7,8 +7,6 @@ type UserRepository interface {
 	GetByAuthData(ctx context.Context, dto *AuthenticateUser) (*User, error)
 	GetAll(ctx context.Context) ([]*User, error)
 	GetByID(ctx context.Context, id int) (*User, error)
-	ExistsByNickname(ctx context.Context, nickname string) bool
-	ExistsByEmail(ctx context.Context, email string) bool
 	Update(ctx context.Context, id int, dto *UpdateUserGeneralInfo) error
 	UpdatePassword(ctx context.Context, id int, password string) error
 	UpdateEmail(ctx context.Context, id int, email string) error
